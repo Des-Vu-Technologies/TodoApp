2f696ce19726b4a84189e4937d1f619f7270f0e0
Akash Dev <107734937+AkashDev23@users.noreply.github.com>
2023-10-18 21:35:39 +0530

    Update todo.js

--- Src/todo.js
+++ Src/todo.js
@@ -1,58 +1,51 @@
-
 const TodoList = [];
 
-function renderTodo(){
-    let totalList = '';
-    for(i=0; i<TodoList.length; i++){
-        const todoObj = TodoList[i];
-        // const name = todo.name;
-        // const dueDate = todo.dueDate;
+function renderTodo() {
+  let totalList = '';
+  for (let i = 0; i < TodoList.length; i++) {
+    const todoObj = TodoList[i];
+    const { name, dueDate, priority } = todoObj;
 
-        // const {name} = todo;
-        // const {dueDate} = todo;
-        const {name, dueDate} = todoObj;
+    const html = `
+      <div class="todo-grid">
+        <div><strong>Name:</strong> ${name}</div>
+        <div><strong>Due Date:</strong> ${dueDate}</div>
+        <div><strong>Priority:</strong> ${priority}</div>
+        <button class="deltBtn" onclick="confirmDelete(${i})">Delete</button>
+      </div>`;
+    totalList += html;
+  }
+  document.querySelector('.js-todoList').innerHTML = totalList;
+}
 
-        const html = `
-        <div class="todo-grid">
-        <div>${name} </div>
-        <div>${dueDate}</div>
-       <button class="deltBtn" onclick="confirmDelete(${i})">Delete</button></div>`;
-        totalList += html;
-        
-    }
-    document.querySelector('.js-todoList').innerHTML = totalList;
-    };
-    
-    function confirmDelete(index) {
-      const confirmation = confirm("Are you sure you want to delete this todo?");
-      if (confirmation) {
-        TodoList.splice(index, 1);
-        renderTodo();
-      }
-    }
+function confirmDelete(index) {
+  const confirmation = confirm("Are you sure you want to delete this todo?");
+  if (confirmation) {
+    TodoList.splice(index, 1);
+    renderTodo();
+  }
+}
 
-    function addInput(){
-        const inputData = document.querySelector('.js-input');
-        const inputDate = document.querySelector('.js-dueDate');
-        let name = inputData.value;
-        let dueDate = inputDate.value;
+function addInput() {
+  const inputData = document.querySelector('.js-input');
+  const inputDate = document.querySelector('.js-dueDate');
+  const prioritySelect = document.querySelector('.js-priority'); // Get the selected priority
 
-        
+  let name = inputData.value;
+  let dueDate = inputDate.value;
+  let priority = prioritySelect.value;
 
-        if (name === '' || dueDate === ''){
-            alert('Enter valid todoName and Date. !!');
-            console.log( typeof dueDate);
-        }
-        else{
-        TodoList.push(
-        {//name:name, 
-        //dueDate:dueDate  
-        name , dueDate});
-        // inputData.value = '';
-        // inputDate.value='';
-        renderTodo();
-        inputData.value = '';
-        inputDate.value='';
-        console.log(TodoList);}
-        
-    };   
+  if (name === '' || dueDate === '' || priority === 'default') {
+    alert('Enter a valid todo name, date, and select a priority.');
+  } else {
+    TodoList.push({
+      name,
+      dueDate,
+      priority,
+    });
+    renderTodo();
+    inputData.value = '';
+    inputDate.value = '';
+    prioritySelect.value = 'default'; // Reset the priority dropdown to the default value
+  }
+}
